0305c12c5dee948e57b0697b41d71ae4741f9d62
Jesse <48510049@qq.com>
2024-02-24 23:22:34 +0800

    public network

--- client/client_udp.py
+++ client/client_udp.py
@@ -1,6 +1,6 @@
 import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_address = ("127.0.0.1", 31999)
+client_address = ("0.0.0.0", 31999)
 server_address = ("127.0.0.1", 31888)
 
 client_socket.bind(client_address)
--- server/udp_server.py
+++ server/udp_server.py
@@ -1,6 +1,6 @@
 import socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ("127.0.0.1", 31888)
+server_address = ("0.0.0.0", 31888)
 
 server_socket.bind(server_address)
 
