9cdd9a11cb611eea365f6e69211ad2428e15db64
jesse <ruixiang.chen@bizops.com.cn>
2024-02-27 18:19:39 +0800

    send file

--- client/client_udp.py
+++ client/client_udp.py
@@ -1,6 +1,7 @@
+import json
 import socket
 import common.constants as constants
-from common.CommandPayload import CommandPayload
+from common.CommandPayload import CommandPayload, from_str
 import time
 import threading
 import uuid
@@ -9,21 +10,31 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_address = ("0.0.0.0", 31999)
 
 server_address = ("0.0.0.0", 31888)
+uuid_index = 0
 device_uuid = uuid.uuid4()
+
+connection_info = {}
+lock = threading.Lock()
+
 
 def keep_alive():
     while True:
-        command = CommandPayload(constants.keep_alive_client_message, device_uuid)
+        command = CommandPayload(constants.keep_alive_client_message, ++uuid_index)
         client_socket.sendto(command.toResponse().encode("utf-8"), server_address)
         data, _ = client_socket.recvfrom(1024)
-        print(data.decode("utf-8"))
+        response_payload = from_str(data.decode("utf-8"))
+        content = eval(response_payload.payload)
+        lock.acquire()
+        connection_info = content
+        lock.release()
         time.sleep(5)
-
 
 client_socket.bind(client_address)
 
-
 keep_alive_thread = threading.Thread(target=keep_alive, name='keep-alive-thread')
 keep_alive_thread.start()
-keep_alive_thread.join()
 
+
+
+
+keep_alive_thread.join()
